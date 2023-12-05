539f2f72dc4ea2b2148b0304ba518725a3018567
alannarisse <alannarisse@users.noreply.github.com>
2023-12-04 20:10:13 -0800

    added slideshow

--- ktm.website/script.js
+++ ktm.website/script.js
@@ -1,23 +1,26 @@
 const slideList = [
     {
     id: 1,
-    text: 'first slide',
-    src: 'http://placekitten.com/600/360'
+    text1: 'As sharp, as ever!', //this is the h3
+    text2: '2023 KTM 1290 SUPER DUKE R EVO', //this is the h2
+    src: 'images/moto-slide01.png'
   },
   {
     id: 2,
-    text: 'second slide',
-    src: 'http://placekitten.com/600/361'
+    text1: 'second slide',
+    text2: 'h2 copy',
+    src: 'images/moto-slide02.png'
   },
   {
     id: 3,
-    text: 'third slide',
-    src: 'http://placekitten.com/600/362'
+    text1: 'third slide',
+    text2: 'h2 copy',
+    src: 'images/moto-slide03.png'
   },
 ]
 
 let count = 0;
-let numSlides = 2;
+let numSlides = 3;
 
 
 function increaseCount() { 
@@ -55,7 +58,8 @@ function decreaseCount() {
 function renderSlide(count) { 
   $('#slideContent').html(`
       <!-- id: ${slideList[count].id} -->
-      <h2>${slideList[count].text}</h2>
+      <h3>${slideList[count].text1}</h3>
+      <h2>${slideList[count].text2}</h2>
       <img class="slideimg" src=${slideList[count].src}>
   `)
 }
